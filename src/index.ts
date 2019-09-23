155cadd2ba665630093a23b20afd6ceea0d2d5b5
Ron Buckton <ron.buckton@microsoft.com>
2019-09-22 22:10:59 -0700

    commit

---
+++ src/index.ts
@@ -0,0 +1,134 @@
+/** Describe Class1 */
+export declare class Class1 {
+
+    /** Describe Field1 */
+    Field1: number;
+}
+
+/** Describe Interface1 */
+export interface Interface1 {
+
+    /** Describe Field2 */
+    Field2: number;
+}
+
+/** Describe Function1 */
+export declare function Function1(): void;
+
+/** Describe Namespace1 */
+export namespace Namespace1 {
+
+    /** Describe Class2 */
+    export declare class Class2 {
+
+        /** Describe Field3 */
+        Field3: number;
+    }
+
+    /** Describe Interface2 */
+    export interface Interface2 {
+
+        /** Describe Field4 */
+        Field4: number;
+    }
+
+    /** Describe Function2 */
+    export declare function Function2(): void;
+}
+
+/** Describe Namespace2 */
+export namespace Namespace2 {
+
+    /** Describe Namespace3 */
+    export namespace Namespace3 {
+
+        /** Describe Class3 */
+        export declare class Class3 {
+
+            /** Describe Field5 */
+            Field5: number;
+        }
+
+        /** Describe Interface3 */
+        export interface Interface3 {
+
+            /** Describe Field6 */
+            Field6: number;
+        }
+
+        /** Describe Function3 */
+        export declare function Function3(): void;
+    }
+}
+
+/** Describe Namespace4 */
+export namespace Namespace4 {
+
+    /** Describe Namespace5 */
+    export namespace Namespace5 {
+
+        /** Describe Class4 */
+        export declare class Class4 {
+
+            /** Describe Field7 */
+            Field7: number;
+        }
+
+        /** Describe Interface4 */
+        export interface Interface4 {
+
+            /** Describe Field8 */
+            Field8: number;
+        }
+
+        /** Describe Function4 */
+        export declare function Function4(): void;
+    }
+
+    /** Describe Class5 */
+    export declare class Class5 {
+
+        /** Describe Field9 */
+        Field9: number;
+    }
+
+    /** Describe Interface5 */
+    export interface Interface5 {
+
+        /** Describe Field10 */
+        Field10: number;
+    }
+
+    /** Describe Function5 */
+    export declare function Function5(): void;
+}
+
+/** Describe Merge (class) */
+export declare class Merge {
+
+    /** Describe Field11 */
+    Field11: number;
+}
+
+/** Describe Merge (interface) */
+export interface Merge {
+
+    /** Describe Field12 */
+    Field12: number;
+}
+
+/** Describe Merge (namespace) */
+export namespace Merge {
+
+    /** Describe Function2 */
+    export declare function Function6(): void;
+}
+
+export namespace A {
+    export enum B {
+        X
+    }
+    export namespace B {
+        export class C {}
+    }
+}
